82a6bbb1f7d560e7f853065f583f68355eaa624f
DanielCogiel <danicog433@student.polsl.pl>
2022-12-01 18:04:21 +0100

    usunięcie komentarza

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpXsrfTokenExtractor } from '@angular/common/http'
 import { Observable } from 'rxjs';
 
-//TEST COMM asdaddasdohsd
-
 @Injectable({
   providedIn: 'root'
 })
